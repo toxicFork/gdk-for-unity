41328e53db88a0f8e3dac4380aa5d1f1672d42fc
Danail Penev <danail@improbable.io>
2018-09-20 17:10:09 +0100

    Clearer variable name

--- workers/unity/Packages/com.improbable.gdk.tools/.DownloadCoreSdk/Program.cs
+++ workers/unity/Packages/com.improbable.gdk.tools/.DownloadCoreSdk/Program.cs
@@ -22,7 +22,7 @@ namespace Improbable
             var schemaStdLibDir = Path.GetFullPath(args[2]);
             var nativeDependenciesPath = Path.GetFullPath("./Assets/Plugins/Improbable/Core");
             var managedDependenciesPath = Path.GetFullPath("./Assets/Plugins/Improbable/Sdk");
-            var newPath = Path.GetFullPath("./Assets/Plugins");
+            var pluginsPath = Path.GetFullPath("./Assets/Plugins");
             var tempPath = Path.GetFullPath($"./build/CoreSdk/{coreSdkVersion}");
 
             var packages = new List<Package>
@@ -33,7 +33,7 @@ namespace Improbable
                 new Package(tempPath, "worker_sdk", "c-static-fullylinked-arm-clang_libcpp-ios", $"{nativeDependenciesPath}/iOS/arm", new List<string> {"include", "libworker_static_fullylinked.a.pic", "worker_static_fullylinked.lib"}),
                 new Package(tempPath, "worker_sdk", "c-static-fullylinked-x86_64-clang_libcpp-ios", $"{nativeDependenciesPath}/iOS/x86_64", new List<string> {"include", "libworker_static_fullylinked.a.pic", "worker_static_fullylinked.lib"}),
                 new Package(tempPath, "worker_sdk", "csharp_core", $"{managedDependenciesPath}/OSX"),
-                new Package(tempPath, "worker_sdk", "csharp_core-static", $"{newPath}/iOS"),
+                new Package(tempPath, "worker_sdk", "csharp_core-static", $"{pluginsPath}/iOS"),
                 new Package(tempPath, "schema", "standard_library", schemaStdLibDir),
                 new Package(tempPath, "tools", "schema_compiler-x86_64-win32", $"{tempPath}/schema_compiler", null, OSPlatform.Windows),
                 new Package(tempPath, "tools", "schema_compiler-x86_64-macos", $"{tempPath}/schema_compiler", null, OSPlatform.OSX),
