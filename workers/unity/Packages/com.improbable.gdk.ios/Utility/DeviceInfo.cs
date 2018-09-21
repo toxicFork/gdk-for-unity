ae4ff71f27bb7aa2ca92be8d7b12071b6fbb5407
Georgiy Kassabli <georgiykassabli@improbable.io>
2018-09-21 16:03:13 +0100

    Add iOS Sim detection and correct project setting

--- workers/unity/Packages/com.improbable.gdk.ios/Utility/DeviceInfo.cs
+++ workers/unity/Packages/com.improbable.gdk.ios/Utility/DeviceInfo.cs
@@ -1,10 +1,12 @@
+using UnityEngine;
+
 namespace Packages.com.improbable.gdk.ios.Utility
 {
     public class DeviceInfo
     {
         public static bool IsIosSimulator()
         {
-            return true;
+            return SystemInfo.deviceModel.Equals("x86_64");
         }
     }
 }
