7a9f9c0e8c8aaad1cc2d4495aee33be7d3a2037f
Georgiy Kassabli <georgiykassabli@improbable.io>
2018-09-27 11:28:40 +0100

    Adding iOS scene and fixing iOS build

---
+++ workers/unity/Assets/Playground/Scripts/Worker/IosClientWorkerConnector.cs
@@ -0,0 +1,73 @@
+#if UNITY_IOS
+using Improbable.Gdk.Mobile;
+using Improbable.Gdk.Mobile.Ios;
+using Improbable.Gdk.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Playground.Worker
+{
+    public class IosClientWorkerConnector : MobileWorkerConnector
+    {
+        public GameObject ConnectionPanel;
+        public GameObject Level;
+
+        private GameObject levelInstance;
+        private InputField input;
+        private Button button;
+        private Text errorMessage;
+
+        public void Awake()
+        {
+            input = ConnectionPanel.transform.Find("ConnectInput").GetComponent<InputField>();
+            button = ConnectionPanel.transform.Find("ConnectButton").GetComponent<Button>();
+            errorMessage = ConnectionPanel.transform.Find("ConnectionError").GetComponent<Text>();
+        }
+
+        public void Start()
+        {
+            button.onClick.AddListener(Connect);
+        }
+
+        public async void Connect()
+        {
+            await Connect(WorkerUtils.UnityClient, new ForwardingDispatcher()).ConfigureAwait(false);
+        }
+
+        protected override void HandleWorkerConnectionEstablished()
+        {
+            WorkerUtils.AddClientSystems(Worker.World);
+            if (Level == null)
+            {
+                return;
+            }
+
+            levelInstance = Instantiate(Level, transform);
+            levelInstance.transform.SetParent(null);
+
+            ConnectionPanel.SetActive(false);
+        }
+
+        protected override void HandleWorkerConnectionFailure()
+        {
+            errorMessage.text = "Connection failure";
+        }
+
+        private string GetIpFromField()
+        {
+            return input.text;
+        }
+
+        protected override string GetHostIp()
+        {
+            var hostIp = GetIpFromField();
+            if (DeviceInfo.IsIosSimulator() && hostIp.Equals(string.Empty))
+            {
+                return RuntimeConfigDefaults.ReceptionistHost;
+            }
+
+            return hostIp;
+        }
+    }
+}
+#endif
