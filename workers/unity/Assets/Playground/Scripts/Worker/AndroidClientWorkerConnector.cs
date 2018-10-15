916c9225cd6b8d4ff350f05f4618acebd253ee8c
Danail Penev <danail@improbable.io>
2018-10-15 14:50:35 +0100

    WIP specific mobile worker types

--- workers/unity/Assets/Playground/Scripts/Worker/AndroidClientWorkerConnector.cs
+++ workers/unity/Assets/Playground/Scripts/Worker/AndroidClientWorkerConnector.cs
@@ -1,13 +1,79 @@
-using System;
+using Improbable.Gdk.Core;
+using Improbable.Gdk.Mobile;
 using UnityEngine;
+using System;
+using UnityEngine.UI;
 #if UNITY_ANDROID
 using Improbable.Gdk.Mobile.Android;
+
 #endif
 
 namespace Playground.Worker
 {
-    public class AndroidClientWorkerConnector : AbstractMobileClientWorker
+    public class AndroidClientWorkerConnector : MobileWorkerConnector
     {
+        [SerializeField] private GameObject connectionPanel;
+        [SerializeField] private GameObject level;
+        [SerializeField] private InputField ipAddressInput;
+        [SerializeField] private Button connectButton;
+        [SerializeField] private Text errorMessage;
+
+        private GameObject levelInstance;
+        private bool connected;
+
+        protected string IpAddress => ipAddressInput != null ? ipAddressInput.text : null;
+
+        public void Awake()
+        {
+            ipAddressInput.text = PlayerPrefs.GetString("cachedIp");
+            connectButton.onClick.AddListener(Connect);
+        }
+
+        public async void Connect()
+        {
+            errorMessage.text = "";
+            await Connect(WorkerUtils.AndroidClient, new ForwardingDispatcher()).ConfigureAwait(false);
+        }
+
+        protected override void HandleWorkerConnectionEstablished()
+        {
+            WorkerUtils.AddClientSystems(Worker.World);
+            if (level == null)
+            {
+                return;
+            }
+
+            levelInstance = Instantiate(level, transform);
+            levelInstance.transform.SetParent(null);
+
+            connected = true;
+            connectionPanel.SetActive(false);
+
+            PlayerPrefs.SetString("cachedIp", IpAddress);
+            PlayerPrefs.Save();
+        }
+
+        protected override void HandleWorkerConnectionFailure()
+        {
+            errorMessage.text = "Connection failure. Please check the IP address";
+        }
+
+        public override void Dispose()
+        {
+            if (levelInstance != null)
+            {
+                Destroy(levelInstance);
+            }
+
+            if (connected)
+            {
+                Destroy(this);
+            }
+
+            Worker?.Dispose();
+            Worker = null;
+        }
+
         protected override string GetHostIp()
         {
 #if UNITY_ANDROID
@@ -19,7 +85,7 @@ namespace Playground.Worker
 
             return hostIp;
 #else
-            throw new NotImplementedException("Incompatible platform: Please use Android");
+            return null;
 #endif
         }
     }
--- workers/unity/Assets/Playground/Scripts/Worker/IosClientWorkerConnector.cs
+++ workers/unity/Assets/Playground/Scripts/Worker/IosClientWorkerConnector.cs
@@ -4,11 +4,75 @@ using UnityEngine;
 using Improbable.Gdk.Mobile.Ios;
 #endif
 using Improbable.Gdk.Core;
+using Improbable.Gdk.Mobile;
+using UnityEngine.UI;
 
 namespace Playground.Worker
 {
-    public class IosClientWorkerConnector : AbstractMobileClientWorker
+    public class IosClientWorkerConnector : MobileWorkerConnector
     {
+        [SerializeField] private GameObject connectionPanel;
+        [SerializeField] private GameObject level;
+        [SerializeField] private InputField ipAddressInput;
+        [SerializeField] private Button connectButton;
+        [SerializeField] private Text errorMessage;
+
+        private GameObject levelInstance;
+        private bool connected;
+
+        protected string IpAddress => ipAddressInput != null ? ipAddressInput.text : null;
+
+        public void Awake()
+        {
+            ipAddressInput.text = PlayerPrefs.GetString("cachedIp");
+            connectButton.onClick.AddListener(Connect);
+        }
+
+        public async void Connect()
+        {
+            errorMessage.text = "";
+            await Connect(WorkerUtils.UnityClient, new ForwardingDispatcher()).ConfigureAwait(false);
+        }
+
+        protected override void HandleWorkerConnectionEstablished()
+        {
+            WorkerUtils.AddClientSystems(Worker.World);
+            if (level == null)
+            {
+                return;
+            }
+
+            levelInstance = Instantiate(level, transform);
+            levelInstance.transform.SetParent(null);
+
+            connected = true;
+            connectionPanel.SetActive(false);
+
+            PlayerPrefs.SetString("cachedIp", IpAddress);
+            PlayerPrefs.Save();
+        }
+
+        protected override void HandleWorkerConnectionFailure()
+        {
+            errorMessage.text = "Connection failure. Please check the IP address";
+        }
+
+        public override void Dispose()
+        {
+            if (levelInstance != null)
+            {
+                Destroy(levelInstance);
+            }
+
+            if (connected)
+            {
+                Destroy(this);
+            }
+
+            Worker?.Dispose();
+            Worker = null;
+        }
+
         protected override string GetHostIp()
         {
 #if UNITY_IOS
