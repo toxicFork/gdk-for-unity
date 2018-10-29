3f54b1a8cc96a3bf64831834af9d5fe865428921
Danail Penev <danail@improbable.io>
2018-10-29 14:47:23 +0000

    VirtualJoystick script and VirtualControllers prefab

---
+++ workers/unity/Assets/Playground/Scripts/MonoBehaviours/VirtualJoystick.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    [NonSerialized] public Vector2 InputDirection;
+    [SerializeField] private Image bgImg;
+    [SerializeField] private Image joystickImg;
+
+    private int? lastTouch;
+
+    public void Start()
+    {
+        InputDirection = Vector2.zero;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (!lastTouch.HasValue || eventData.pointerId != lastTouch)
+        {
+            return;
+        }
+
+        if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform, eventData.position,
+            eventData.pressEventCamera, out var pos))
+        {
+            return;
+        }
+
+        pos /= bgImg.rectTransform.sizeDelta;
+
+        var x = bgImg.rectTransform.pivot.x == 1 ? pos.x * 2 + 1 : pos.x * 2 - 1;
+        var y = bgImg.rectTransform.pivot.y == 1 ? pos.y * 2 + 1 : pos.y * 2 - 1;
+
+        InputDirection = new Vector2(x, y);
+        InputDirection = InputDirection.sqrMagnitude > 1 ? InputDirection.normalized : InputDirection;
+
+        joystickImg.rectTransform.anchoredPosition = InputDirection * bgImg.rectTransform.sizeDelta / 3;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (!lastTouch.HasValue || eventData.pointerId != lastTouch.Value)
+        {
+            InputDirection = Vector3.zero;
+            joystickImg.rectTransform.anchoredPosition = Vector3.zero;
+            lastTouch = null;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (lastTouch.HasValue)
+        {
+            return;
+        }
+
+        lastTouch = eventData.pointerId;
+        OnDrag(eventData);
+    }
+}
